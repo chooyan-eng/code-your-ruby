216aac5556d3789f84b2151eac24dce456745be1
gouf <innocent.zero@gmail.com>
2018-02-15 14:46:40 +0900

    Implement number detection of Fizz and Buzz

--- user/gouf/fizzbuzz/lib/fizzbuzz.rb
+++ user/gouf/fizzbuzz/lib/fizzbuzz.rb
@@ -1,10 +1,20 @@
 module FizzBuzzExtension
   refine Numeric do
+    def fizz?
+      modulo(3).zero?
+    end
+
+    def buzz?
+      modulo(5).zero?
+    end
   end
 end
 
 module FizzBuzz
+  using FizzBuzzExtension
+
   def self.detect(n)
-    'Fizz'
+    return 'Fizz' if n.fizz?
+    return 'Buzz' if n.buzz?
   end
 end
