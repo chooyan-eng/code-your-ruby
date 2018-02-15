ea8da5821882b4cfa213c124ba5f2afaf8c00705
gouf <innocent.zero@gmail.com>
2018-02-15 14:51:18 +0900

    Add test non-fizzbuzz numbers

--- user/gouf/fizzbuzz/spec/fizzbuzz_spec.rb
+++ user/gouf/fizzbuzz/spec/fizzbuzz_spec.rb
@@ -28,4 +28,17 @@ describe FizzBuzz do
       include_examples 'FizzBuzz', n, 'FizzBuzz'
     end
   end
+
+  context 'non-fizzbuzz number' do
+    non_divisible = [1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19, 22, 23,
+                     26, 28, 29, 31, 32, 34, 37, 38, 41, 43, 44, 46,
+                     47, 49, 52, 53, 56, 58, 59, 61, 62, 64, 67, 68,
+                     71, 73, 74, 76, 77, 79, 82, 83, 86, 88, 89, 91, 92, 94, 97, 98]
+
+    non_divisible.each do |n|
+      it "will returns #{n} as String" do
+        expect(FizzBuzz.detect(n)).to eq n.to_s
+      end
+    end
+  end
 end
